4a397efaf696c0e0baafe030b926a20a8b2c8691
LautaroAiello <lautaroaiello34@gmail.com>
2024-10-10 09:28:07 -0300

    primer commit

---
+++ src/test/java/com/myteacher/demo/MyTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myteacher.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTeacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
