63d6d2f9fa670e4cde13fa6be1cf9301643ccce9
LautaroAiello <lautaroaiello34@gmail.com>
2024-10-27 21:10:21 -0300

    front end de menus; login y redirecciones realizados

--- src/test/java/com/myteacher/demo/MyTeacherApplicationTests.java
+++ src/test/java/com/myteacher/demo/MyTeacherApplicationTests.java
@@ -3,6 +3,7 @@ package com.myteacher.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class MyTeacherApplicationTests {
 
